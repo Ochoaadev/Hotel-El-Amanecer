c9e77afcbc2297f44dd9621a46f0581ba6fa205c
zeu22 <zeuddy22@gmail.com>
2024-03-05 10:24:07 -0400

    Implementacion de Rutas con React Router

---
+++ Front/src/routes.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { createBrowserRouter } from "react-router-dom";
+import Form_reservas from "./routes/Formulario_Reservas";
+import About from "./components/AboutUs";
+import Service from "./components/services";
+import Advanced from './components/Benf'
+import App from "./App";
+
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <App />,
+    },
+    {
+        path: '/formulario_reservas',
+        element: <Form_reservas/>
+    },
+    {
+        path: '/AboutUs',
+        element: <About/>
+    },
+    {
+        path: '/services',
+        element: <Service/>
+    },
+    {
+        path: '/Benf',
+        element: <Advanced/>
+    }
+])
