560f17345c7c2f0a84bdb0e04ba96d25a8055e68
Jupagolab <jupagolab@gmail.com>
2024-03-05 02:36:26 -0400

    Dependecias, configuraciones de tailwindcss

---
+++ Front/tailwind.config.js
@@ -0,0 +1,38 @@
+/** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        '4xl': '2rem',
+        '5xl': '2.5rem',
+      }
+    },
+    colors: {
+      blanco: "#FFFFFF",
+      claro: "#F4EDEA",
+      azul: "#12263A",
+      verde: '#06BCC1',
+      gris: '#C5D8D1',
+      crema:"#F4D1AE",
+      marron:"#E68F51",
+      oscuro:"#3c160e",
+      transparent: 'transparent',
+      current: 'currentColor',
+      black: colors.black,
+      white: colors.white,
+      gray: colors.slate,
+      green: colors.emerald,
+      purple: colors.violet,
+      yellow: colors.amber,
+      pink: colors.fuchsia,
+    },
+  },
+  plugins: [],
+}
