e5c6ff7d42fefdd791f1e34e0a4961b3fcee00ac
Jupagolab <jupagolab@gmail.com>
2024-03-05 10:41:26 -0400

    Home Page

---
+++ Front/src/routes/MiHotel.jsx
@@ -0,0 +1,23 @@
+import Header from "../components/Header"
+import Info from '../components/Info'
+import Advanced from '../components/Benf'
+import Footer from '../components/Footer'
+import Carrusel from '../components/carrusel'
+import { useLocation } from "react-router-dom";
+import { useAuth } from "../contexts/AuthProvider";
+
+function MiHotel() {
+  const location = useLocation();
+  const { user } = useAuth();
+  return (
+    <>
+      <Header />
+      <Info />
+      <Advanced />
+      <Carrusel />
+      <Footer />
+    </>
+  );
+}
+
+export default MiHotel;
